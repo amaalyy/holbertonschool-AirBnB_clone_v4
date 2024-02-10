4fb543bd14bdacbad7bbcdadb486020204bb8bfc
amaalyy <amal.elzowawi@gmail.com>
2024-02-10 17:44:41 +0100

    update

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).prop('checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    let text = '';
+    for (const amenity in amenities) {
+      text += amenities[amenity] + ', ';
+    }
+    text = text.slice(0, -2);
+    $('div.amenities h4').text(text);
+  });
+});
